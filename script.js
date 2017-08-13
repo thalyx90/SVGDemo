a23dacdfdebbd9986dc9eba0eef272e4db96c250
Trung Ly <trung.ly@cit3-101m-01.auckland.natcoll.ac.nz>
2017-08-14 10:23:44 +1200

    Adding Australia to the dataset

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ var data = [
 			{name:'UK',value:50},
 			{name:'France',value:40},
 			{name:'Russia',value:60},
-			{name:'NZ',value:80}
+			{name:'NZ',value:80},
+			{name:'Australia',value:80}
 		];
-
