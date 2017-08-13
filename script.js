a7a4cc973a4d18526e8173a3d5c1fd86f9d9eb7f
Trung Ly <trung.ly@cit3-101m-01.auckland.natcoll.ac.nz>
2017-08-14 10:12:29 +1200

    Starting project

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var marginTop = 50;
+var marginLeft = 50;
+var width = 600;
+var height = 300;
+
+var data = [
+			{name:'UK',value:50},
+			{name:'France',value:40},
+			{name:'Russia',value:60},
+			{name:'NZ',value:80}
+		];
+
